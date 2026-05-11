8442cec9444958e4167adef41be56d958cc4be17
Madhav Garibnawaz <madhavgaribnavaz123@gmail.com>
2026-05-11 13:16:23 +0530

    Final changes

---
+++ admin/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyDreTWI7KI_A2CgQ94Cvyq_TVuDE4CDylQ",
+    authDomain: "fir-e340a.firebaseapp.com",
+    projectId: "fir-e340a",
+    storageBucket: "fir-e340a.firebasestorage.app",
+    messagingSenderId: "699877494764",
+    appId: "1:699877494764:web:5849b50c3e1c2a31f8fac9"
+  }
+};
